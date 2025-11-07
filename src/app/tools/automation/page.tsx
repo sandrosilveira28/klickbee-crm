1252716fe7e13daec5392ccb9c9a131c690af26c
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-11-07 15:07:59 +0100

    feat: add ComingSoonPage component and integrate it into ToolautomationPage

---
+++ src/app/tools/automation/page.tsx
@@ -0,0 +1,7 @@
+import ComingSoonPage from "@/components/ComingSoon";
+
+export default function ToolautomationPage() {
+    return (
+        <ComingSoonPage/>
+    )
+}
---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ComingSoonPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full py-20">
+            <h1 className="text-4xl font-bold mb-4">Coming Soon!</h1>
+            <p className="text-lg text-gray-600">We're working hard to bring you this feature. Stay tuned!</p>
+        </div>
+    );
+}
